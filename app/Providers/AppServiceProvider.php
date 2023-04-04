13d4b1b7a190c0d4691108da9bc0ff4bd5e64bce
Ahmed Zaky <a.zaki@benefits-hero.com>
2023-04-05 00:52:18 +0200

    mini enhancement

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,30 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Logged in as a user
+        $this->app->singleton('user', function () {
+            if ($user = optional(auth())->user())
+                return $user;
+            
+            return null;
+        });
+
+        // Logged in as an admin
+        $this->app->singleton('admin', function () {
+            if ($admin = optional(auth("admin"))->user()) {
+
+                return $admin;
+            }
+
+            return null;
+        });
+
+
+        // Basic monitoring for db queries to check the performance
+        DB::listen(function ($query) {
+            if ($query->time > 1000) {
+                Log::warning("An Individual database Query exceeded 1 second. Sql: ".$query->sql);
+            }
+        });
     }
 }
